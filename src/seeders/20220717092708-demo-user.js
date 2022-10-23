db30318d4b347286d7aa3a29e0308214ef449039
DoNgocSon <ngocsonse2001@gmail.com>
2022-10-23 15:34:46 +0700

    init project

---
+++ src/seeders/20220717092708-demo-user.js
@@ -0,0 +1,24 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("Users", [
+      {
+        email: "admin@gmail.com",
+        password: "123",
+        firstName: "Do",
+        lastName: "Son",
+        gender: 1,
+        address: "Hà Nội",
+        typeRole: "ROLE",
+        keyRole: "R1",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Users", null, {});
+  },
+};
